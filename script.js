410748968956ff1bdd63f7de39ec29918262dbeb
Supriya Limbole <127923605+BoonDocks-sl@users.noreply.github.com>
2023-05-13 10:12:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // complete the given function
 
 function palindrome(str){
+	// convert the input string to lowercase and remove all non-alphanumeric characters
+  let cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+
+  // reverse the cleaned string and compare it to the original string
+  let reversedStr = cleanStr.split('').reverse().join('');
+  return cleanStr === reversedStr;
 
 }
 module.exports = palindrome
